8c8d89dac0ed32b3ce73fd7b6e5623c6a43e5317
Imran Rasheed <imranrasheed.developer@outlook.com>
2022-07-25 14:14:16 +0500

    bug fixed

--- app/src/main/java/split/com/app/ui/main/view/promotion/Promotion.java
+++ app/src/main/java/split/com/app/ui/main/view/promotion/Promotion.java
@@ -39,9 +39,7 @@ public class Promotion extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        if (!Constants.Coins.isEmpty()){
-            binding.tvTotalCoins.setText(Constants.Coins + " Coins");
-        }
+
         initClickListeners();
     }
 
